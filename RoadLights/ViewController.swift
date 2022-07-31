ddc8cac66420a9b4905804d0b6eebeb74849985a
Leo Fridman <madfridman@gmail.com>
2022-07-31 17:49:23 +0200

    Added rounded views

--- RoadLights/ViewController.swift
+++ RoadLights/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var orangeLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 90
+        orangeLightView.layer.cornerRadius = 90
+        greenLightView.layer.cornerRadius = 90
+        redLightView.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+        orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
+        greenLightView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redLightView.backgroundColor == UIColor.red.withAlphaComponent(0.5) && orangeLightView.backgroundColor == UIColor.orange.withAlphaComponent(0.5) &&   greenLightView.backgroundColor == UIColor.green.withAlphaComponent(0.5){
+            redLightView.backgroundColor = UIColor.red.withAlphaComponent(1)
+        } else if redLightView.backgroundColor == UIColor.red.withAlphaComponent(1) {
+            redLightView.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+            orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(1)
+        } else if  orangeLightView.backgroundColor == UIColor.orange.withAlphaComponent(1) {
+            orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
+            greenLightView.backgroundColor = UIColor.green.withAlphaComponent(1)
+        } else if greenLightView.backgroundColor == UIColor.green.withAlphaComponent(1) {
+            greenLightView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+            redLightView.backgroundColor = UIColor.red.withAlphaComponent(1)
+        }
+    }
+    
+    
+    
 }
 
