a6387684e74a82115c47061cbca92b442d58a38c
Leo Fridman <madfridman@gmail.com>
2022-08-01 20:04:09 +0200

    Code optimized and new constraints

--- RoadLights/ViewController.swift
+++ RoadLights/ViewController.swift
@@ -12,33 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet var redLightView: UIView!
     @IBOutlet var orangeLightView: UIView!
     @IBOutlet var greenLightView: UIView!
+    
     @IBOutlet var startButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.layer.cornerRadius = 90
-        orangeLightView.layer.cornerRadius = 90
-        greenLightView.layer.cornerRadius = 90
-        redLightView.backgroundColor = UIColor.red.withAlphaComponent(0.5)
-        orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
-        greenLightView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        orangeLightView.layer.cornerRadius = orangeLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        redLightView.alpha = 0.5
+        orangeLightView.alpha = 0.5
+        greenLightView.alpha = 0.5
+        startButton.layer.cornerRadius = 10
     }
     
     @IBAction private func startButtonPressed() {
         startButton.setTitle("Next", for: .normal)
         
-        if redLightView.backgroundColor == UIColor.red.withAlphaComponent(0.5) && orangeLightView.backgroundColor == UIColor.orange.withAlphaComponent(0.5) &&   greenLightView.backgroundColor == UIColor.green.withAlphaComponent(0.5){
-            redLightView.backgroundColor = UIColor.red.withAlphaComponent(1)
-        } else if redLightView.backgroundColor == UIColor.red.withAlphaComponent(1) {
-            redLightView.backgroundColor = UIColor.red.withAlphaComponent(0.5)
-            orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(1)
-        } else if  orangeLightView.backgroundColor == UIColor.orange.withAlphaComponent(1) {
-            orangeLightView.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
-            greenLightView.backgroundColor = UIColor.green.withAlphaComponent(1)
-        } else if greenLightView.backgroundColor == UIColor.green.withAlphaComponent(1) {
-            greenLightView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
-            redLightView.backgroundColor = UIColor.red.withAlphaComponent(1)
+        if redLightView.alpha == 1 {
+            redLightView.alpha = 0.5
+            orangeLightView.alpha = 1
+        } else if orangeLightView.alpha == 1 {
+            orangeLightView.alpha = 0.5
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.5
+            redLightView.alpha = 1
+        } else {
+            redLightView.alpha = 1
         }
     }
     
